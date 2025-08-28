f6c1a0e5a4e523cd51512d115cb711ff74687a99
KenAgnas <163096073+KenAgnas@users.noreply.github.com>
2025-08-28 20:16:58 +0800

    Add files via upload

---
+++ php_practice.php
@@ -0,0 +1,69 @@
+<!-- Hello!
+<h1>Hello!</h1>
+<style>
+    h1 {
+        color: yellow;
+    }
+    </style>
+    <script>
+        alert("Hello!")
+        </script> -->
+        <?php
+        /*
+        echo "<b>Hello World!</b>"
+        */
+        ?>
+        
+
+        
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <!-- <h1>Hello This is Me!</h1> -->
+        <?php
+      //  echo "Hello!";
+
+       // $color = "Red";
+       // $color = "Blue";
+       // echo $color;
+       // echo $color;\
+
+        $_ = 5;
+
+       // echo 4-5
+
+      // var_dump($color);
+      // var_dump($_);
+      // var_dump(3.1);
+      //var_dump(false);
+       // var_dump([1.2]);
+
+        //$x = 5; //global
+
+       // function test(){
+         //$x = 10; //local
+        // echo = $x;
+         //echo "<br>";
+        //}
+       // test();
+        //test();
+
+       //echo ("Hello"), ("Hello"), ("Hello!"); 
+        //echo "World!";
+        //print ("Hello");
+       // print "World!";
+
+echo "Hello" . "World!";
+
+$x = "Hello";
+$y = "Love";
+
+echo = $x . " " . $y, "<br>";
+        ?>
+    </body>
+    </html>
